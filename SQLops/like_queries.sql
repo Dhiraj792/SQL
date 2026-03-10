18681a8beee48b83280774398f690c05c6b926e1
D <dhirajbadre@gmail.com>
2026-03-10 23:39:32 +0530

    Add files via upload

---
+++ SQLops/like_queries.sql
@@ -0,0 +1,7 @@
+SELECT * FROM employees.employees;
+select * from employees where first_name like 'e%';
+select * from employees where first_name like 'eb%';
+select * from employees where first_name like '%en';
+select * from employees;
+-- if we want like second letter of name is want a or any thing that time we use _(underscore)
+select * from employees where last_name like '_a%';                                                                 
