65d67e6ff12735c52384e5a252636aedc93c6eac
D <dhirajbadre@gmail.com>
2026-03-12 00:04:25 +0530

    Update Distinct clause.sql

--- ORDER BY & DISTINCT ClAUSE/Distinct clause.sql
+++ ORDER BY & DISTINCT ClAUSE/Distinct clause.sql
@@ -1,1 +1,11 @@
+-- DISTINCT clause- To remove duplicate values 
 -- DISTINCT clause- To remove duplicates values 
+USE classicmodels;
+select * from employees;
+select lastName from employees order by lastName;
+select distinct lastName from employees order by lastName;
+select * from customers;
+select state from customers;
+select distinct state from customers;
+select distinct state,city from customers where state is not null;
+
