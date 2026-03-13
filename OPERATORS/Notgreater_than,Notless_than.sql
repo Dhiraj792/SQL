8b7d6b14851107191d310a9cb93f72f38b1009d5
D <dhirajbadre@gmail.com>
2026-03-13 23:57:23 +0530

    Add files via upload

---
+++ OPERATORS/Notgreater_than,Notless_than.sql
@@ -0,0 +1,5 @@
+select * from customers where creditLimit>70000;
+select * from customers where NOT creditLimit>70000;
+select * from customers where creditLimit<60000;
+select * from customers where NOT creditLimit<60000;
+              
