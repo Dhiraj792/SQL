58dd12c93edfdbef586fec2fe4b0f1bee8a29ef9
D <dhirajbadre@gmail.com>
2026-03-13 23:38:26 +0530

    Update AND ,OR Operator.sql

--- OPERATORS/AND ,OR Operator.sql
+++ OPERATORS/AND ,OR Operator.sql
@@ -7,3 +7,10 @@ from
 where(country='USA'
 		or country='France')
         and creditLimit>100000;
+select 
+	customerName,
+	country,
+	creditLimit
+from 
+	customers
+where creditLimit between 70000 and 85000;
