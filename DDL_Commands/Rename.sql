e5464aa689b48dc7d443b3c7058de5c73119777a
D <dhirajbadre@gmail.com>
2026-03-15 19:30:34 +0530

    Add files via upload

---
+++ DDL_Commands/Rename.sql
@@ -0,0 +1,5 @@
+-- RENAME command used to chnage the name of database table 
+use demodb;
+rename table employee to EMPTable;
+-- we also can use alter command to rename the table
+alter table student rename to students;
