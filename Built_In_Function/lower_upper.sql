6cf007c28e5c4babc1903ac781d9d4636986354a
D <dhirajbadre@gmail.com>
2026-03-18 23:51:43 +0530

    Add files via upload

---
+++ Built_In_Function/lower_upper.sql
@@ -0,0 +1,6 @@
+select  lower('ORACLE');
+USE classicmodels;
+select * from employees;
+select firstName from employees;
+select lower(firstName) from employees;
+select upper(firstName) from employees;
