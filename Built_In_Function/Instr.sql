28ae6319a2c930440167d98495c8076a9d3b04c7
D <dhirajbadre@gmail.com>
2026-03-18 23:49:57 +0530

    Add files via upload

---
+++ Built_In_Function/Instr.sql
@@ -0,0 +1,5 @@
+-- INSTR:- RETURNS THE POSITION OF FIRST OCCURRENCE OF A SUBSTRING IN A STRING 
+ SELECT instr("This is MYSQl","MYSQL");
+  SELECT instr("This is MYSQl","this");
+  select * from products;
+  select * from products where instr(productLine,'cars')>0;
