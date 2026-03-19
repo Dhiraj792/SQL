dc79d192bbc7d2f1500b6f04acbad58b6f1e5041
D <dhirajbadre@gmail.com>
2026-03-19 20:19:03 +0530

    Add files via upload

---
+++ Built_In_Function/Mathematical_Numeric functions/Mathematical_Numeric functions.sql
@@ -0,0 +1,9 @@
+-- ABS():- returns the absolute value of a number
+select abs(-234.654);
+select mod(13,5);
+select 13%5;
+select round(23.45,1);
+select truncate(1.5555,2);
+select round(1.5555,2);
+select truncate(123421,-2);
+select sqrt(24);
