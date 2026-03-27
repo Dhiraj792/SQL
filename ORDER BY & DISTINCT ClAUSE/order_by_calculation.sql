70d1e1e3fc2149b08b5eb5f4d2fea217fb4a4ced
D <dhirajbadre@gmail.com>
2026-03-27 23:25:11 +0530

    Update order_by_calculation.sql

--- ORDER BY & DISTINCT ClAUSE/order_by_calculation.sql
+++ ORDER BY & DISTINCT ClAUSE/order_by_calculation.sql
@@ -1,5 +1,5 @@
 USE classicmodels;
--- order by use in calculation way
+-- order by use in calculation way returns values according to order
 select * from orderdetails;
 select orderNumber,orderLineNumber,quantityOrdered*priceEach from orderdetails ;
 select orderNumber orderno,orderLineNumber orderlineno,quantityOrdered*priceEach total_amount from orderdetails order by quantityOrdered*priceEach;
