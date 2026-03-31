6dbab41bb6bc942f2a9df8930d0ad11c54a7f6ff
D <dhirajbadre@gmail.com>
2026-03-31 23:48:06 +0530

    Refactor SQL query for better readability

--- Joins/right_join.sql
+++ Joins/right_join.sql
@@ -1,8 +1,15 @@
   use d;
   select * from t1 right join t2 on t1.num=t2.num;
   use classicmodels;
-  select employeeNumber customerName,cumstomerNumber from customer right join employees on 
+  select 
+    employeeNumber customerName,
+    cumstomerNumber 
+  from 
+    customer 
+  right join 
+    employees on 
   salesRepEmployeeNumber=employeeNumber
-  where customerNumber is null;  
+  where 
+    customerNumber is null;  
   
   
