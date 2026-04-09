5254d456b434ee20e97ea1f1a0f06ef261449018
D <dhirajbadre@gmail.com>
2026-04-10 00:40:41 +0530

    Add files via upload

---
+++ views and indexes/Limit.sql
@@ -0,0 +1,12 @@
+-- limit
+use hrdatabase;
+select * from employees;
+select * from employees limit 5; -- first five rows
+select * from employees limit 2,5; -- form third row next five row;
+
+
+
+
+
+
+
