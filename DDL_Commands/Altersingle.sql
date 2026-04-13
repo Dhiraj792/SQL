a37e07a2c5907c5e63def990113f8d4811c343b1
D <dhirajbadre@gmail.com>
2026-04-13 23:32:47 +0530

    Update Altersingle.sql

--- DDL_Commands/Altersingle.sql
+++ DDL_Commands/Altersingle.sql
@@ -1,7 +1,7 @@
 alter table studentTable ADD DOA datetime default current_timestamp;
 insert into studentTable(Name,DOB,Age,remark) values('Anil','2003-10-21',23,'Coming from good school');
 alter table studentTable modify remark varchar(100);
-alter table studentTable modify Name varchar(5); -- error because of data is more than 5 character1
+alter table studentTable modify Name varchar(5); -- error because of data is more than 5 character
 select * from studentTable; 
 
 
