961249491cfcb7f0a62e71aef761a6ddca0afc3d
D <dhirajbadre@gmail.com>
2026-04-18 23:33:31 +0530

    Update Order_by.sql

--- GroupBy_Having_OrderBy/Order_by.sql
+++ GroupBy_Having_OrderBy/Order_by.sql
@@ -3,5 +3,5 @@ select * from employees order by salary desc;
 select  department_id,sum(salary) tsalary from employees group by department_id order by tsalary desc;
 select department_id,sum(salary) tsalary from employees 
 		where department_id <> 6 
-				group by department_id having tsalary>20000
-					order by tsalary desc;                
+			group by department_id having tsalary>20000
+				order by tsalary desc;                
